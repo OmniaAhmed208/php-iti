b8e6355702dc292873e5b88d9bb439ce0bc2d8d9
OmniaAhmed208 <oa66494@gmail.com>
2022-12-13 23:39:26 +0200

    the slider task updated

--- task11 day7 js/js/slider.js
+++ task11 day7 js/js/slider.js
@@ -1,17 +1,73 @@
 let next = document.querySelector('.next');
 let prev = document.querySelector('.prev');
 let slides= document.querySelectorAll('.container>div');
+let slideLeft= document.querySelectorAll('.left');
+let slideRight= document.querySelectorAll('.right');
+let active= document.querySelector('.active');
+
 let counter = slides.length;
-let currentSlide = 1;
+let currentSlide = active.getAttribute('data-set');
 
 next.onclick = nextFun;
 prev.onclick = prevFun;
+
+function nextFun(){
+    if(next.classList.contains('disabled')){
+        return false;
+    } else{
+        currentSlide--;
+        
+        check();
+    }
+}
+
+function prevFun(){
+    if(prev.classList.contains('disabled')){
+        return false;
+    } else{
+        currentSlide++;
+        
+        check();
+    }
+}
+
+function check(){
+    slides.forEach(slide=>{
+        slide.classList.remove('active');
+    });
+
+    slideLeft.forEach(slide=>{
+        if(slide.getAttribute('data-set') == currentSlide){
+            slide.classList.add('active')
+        }
+        
+        if(currentSlide == 1){
+                next.classList.add('disabled');
+        }   else{
+               next.classList.remove('disabled')
+            }
+    });
+
+    slideRight.forEach(slide=>{
+        if(slide.getAttribute('data-set') == currentSlide){
+            slide.classList.add('active');
+        }
+        
+        if(currentSlide == slides.length){
+                prev.classList.add('disabled');
+        }   else{
+               prev.classList.remove('disabled')
+            }
+    });
+}
+
+// indicators
 
 let ul = document.createElement('ul');
 ul.className = 'ul';
 let ulElement = document.querySelector('.ul');
 
-for(let i=1 ; i<= counter; i++){
+for(let i=1 ; i<= slides.length; i++){
 
     let li = document.createElement('li');
     li.setAttribute('data-index', i);
@@ -28,60 +84,6 @@ for(let i=0; i<arrLi.length; i++){
     arrLi[i].onclick = function(){
 
         currentSlide = arrLi[i].getAttribute('data-index');
-
-        check();
-    }
-}
-
-check();
-
-function check(){
-
-    removeActive();
-
-    slides[currentSlide-1].classList.add('active');
-
-    arrLi[currentSlide-1].classList.add('active');
-
-    if(currentSlide == 1){
-        prev.classList.add('disabled');
-    } else{
-        prev.classList.remove('disabled')
-    }
-
-    if(currentSlide == counter){
-        next.classList.add('disabled');
-    } else{
-        next.classList.remove('disabled')
-    }
-
-}
-
-function removeActive(){
-    slides.forEach((ele)=>{
-        ele.classList.remove('active');
-    });
-
-    arrLi.forEach((ele)=>{
-        ele.classList.remove('active');
-    })
-}
-
-function nextFun(){
-    if(next.classList.contains('disabled')){
-        return false
-    } else{
-        currentSlide++;
-
-        check();
-    }
-}
-
-function prevFun(){
-    if(prev.classList.contains('disabled')){
-        return false
-    } else{
-        currentSlide--;
 
         check();
     }
